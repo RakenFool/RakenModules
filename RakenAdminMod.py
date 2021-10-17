a75f592d9888e9d92258cd94ff00231cf2402db7
RakenFool <92396373+RakenFool@users.noreply.github.com>
2021-10-17 18:26:55 +0300

    Update RakenAdminMod.py

--- RakenAdminMod.py
+++ RakenAdminMod.py
@@ -679,3 +679,48 @@ async def check_media(message, reply):
                 return await message.respond(f" ✅ {user.first_name}<i> был забанен. Чтобы отключить эту функцию отправьте в чат команду</i> <code>.add (юзернейм)</code>")
         except:
             pass
+
+    strings = {"reply_start": "<b>❌Ответьте на сообщение, чтобы отметить где начать.</b>",
+               "invalid_chat": "<b>❌Это не чат.</b>",
+               "needs_admin": "<b>❌ Иди попрошайничать админку. У тебя ее здесь нет.</b>",
+               "recovered": "✅Удаленные сообщения {} восстановлены. Впервые отправлено {} этим человеком {}, удалено {} этим человеком {}"}
+
+    @loader.group_admin
+    @loader.ratelimit
+    async def recoverdeletedcmd(self, message):
+        """Восстановляет удалённые сообщения"""
+        msgs = message.client.iter_admin_log(message.to_id, delete=True)
+        if not message.is_reply:
+            await utils.answer(message, self.strings("reply_start", message))
+            return
+        if not isinstance(message.to_id, telethon.tl.types.PeerChannel):
+            await utils.answer(message, self.strings("invalid_chat", message))
+            return
+        target = (await message.get_reply_message()).date
+        ret = []
+        try:
+            async for msg in msgs:
+                if msg.original.date < target:
+                    break
+                if msg.original.action.message.date < target:
+                    continue
+                ret += [msg]
+        except telethon.errors.rpcerrorlist.ChatAdminRequiredError:
+            await utils.answer(message, self.strings("needs_admin", message))
+        args = utils.get_args(message)
+        if len(args) > 0:
+            try:
+                count = int(args[0])
+                ret = ret[-count:]
+            except ValueError:
+                pass
+        for msg in reversed(ret):
+            orig = msg.original.action.message
+            deldate = msg.original.date.isoformat()
+            origdate = orig.date.isoformat()
+            await message.respond(self.strings("recovered", message).format(msg.id, origdate, orig.from_id,
+                                                                            deldate, msg.user_id))
+            if isinstance(orig, telethon.tl.types.MessageService):
+                await message.respond("<b>" + utils.escape_html(orig.stringify()) + "</b>")
+            else:
+                await message.respond(orig)
