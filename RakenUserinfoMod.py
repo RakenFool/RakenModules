e67342a9835dfd7f55ec4b5d30442efcc688bc2b
RakenFool <92396373+RakenFool@users.noreply.github.com>
2021-11-03 12:06:03 +0200

    Update RakenUserinfoMod.py

--- RakenUserinfoMod.py
+++ RakenUserinfoMod.py
@@ -5,7 +5,6 @@ from telethon.tl.functions.users import GetFullUserRequest
 
 logger = logging.getLogger(__name__)
 
-
 @loader.tds
 class UserInfoMod(loader.Module):
     """Tells you about people"""
