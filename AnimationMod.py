53051243e540be66196b84bab7cc47aacee49a2d
RakenFool <92396373+RakenFool@users.noreply.github.com>
2021-11-21 18:01:58 +0200

    Create AnimationMod.py

---
+++ AnimationMod.py
@@ -0,0 +1,80 @@
+from .. import loader, utils
+import asyncio
+from asyncio import sleep
+
+@loader.tds
+class AnimationMod(loader.Module):
+  strings = {"name": "AnimationMod"}
+  
+  async def client_ready(self, client, db):
+    self.client = client
+    
+  @loader.owner
+  async def heartcmd(self, message):
+    args = utils.get_args_raw(message)
+    try:
+      args = int(args)
+      for _ in range(args):
+              for heart in ['❤️ㅤ', '️🧡ㅤ', '💛ㅤ', '💚ㅤ', '💙ㅤ', '💜ㅤ']:
+                await message.edit(heart)
+                await sleep(0.5)
+    except ValueError:
+      await message.edit("〰️ Не хватает аргументов.")
+          
+  @loader.owner
+  async def clockcmd(self, message):
+    args = utils.get_args_raw(message)
+    try:
+      args = int(args)
+      for _ in range(args):
+        for clock in ['🕛', '🕐', '🕑' ,'🕒', '🕓', '🕔', '🕕', '🕖', '🕗', '🕘', '🕙']:
+          await message.edit(clock)
+          await sleep(0.3)    
+    except ValueError:
+      await message.edit("〰️ Не хватает аргументов.")
+        
+  @loader.owner
+  async def mooncmd(self, message):
+    args = utils.get_args_raw(message)
+    try:
+      args = int(args)
+      for _ in range(args):
+        for moon in ['🌕ㅤ', '🌖ㅤ', '🌗ㅤ', '🌘ㅤ', '🌑ㅤ' ,'🌒ㅤ', '🌓ㅤ', '🌔ㅤ']:
+          await message.edit(moon)
+          await sleep(0.3)
+    except ValueError:
+      await message.edit("〰️ Не хватает аргументов.")
+  async def moonfcmd(self, message):
+    """Используй .moonf <аргументы>"""
+    args = utils.get_args_raw(message)
+    try:
+      args = int(args)
+      for _ in range(args):
+        for moonf in ['🌚', '🌝']:
+          await message.edit(moonf)
+          await sleep(0.3)
+    except ValueError:
+      await message.edit("〰️ Не хватает аргументов.")   
+   @loader.owner
+  async def mooncmd(self, message):
+    args = utils.get_args_raw(message)
+    try:
+      args = int(args)
+      for _ in range(args):
+        for moon in ['🌕ㅤ', '🌖ㅤ', '🌗ㅤ', '🌘ㅤ', '🌑ㅤ' ,'🌒ㅤ', '🌓ㅤ', '🌔ㅤ']:
+          await message.edit(moon)
+          await sleep(0.3)
+    except ValueError:
+      await message.edit("〰️ Не хватает аргументов.")
+  async def portalcmd(self, message):
+    """Используй .portal <аргументы>"""
+    args = utils.get_args_raw(message)
+    try:
+      args = int(args)
+      for _ in range(args):
+        for portal in ['🕳 \n \n \n 🕳', '🕳 \n 🎾 \n \n 🕳', '🕳 \n \n 🎾 \n 🕳']:
+          await message.edit(portal)
+          await sleep(0.3)
+    except ValueError:
+      await message.edit("〰️ Не хватает аргументов.")    
+  
