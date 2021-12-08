3b988401ecf5e2c1f6d557e6c197643b72a6ba61
RakenFool <92396373+RakenFool@users.noreply.github.com>
2021-12-08 22:03:39 +0200

    Create DotMod.py

---
+++ DotMod.py
@@ -0,0 +1,45 @@
+from .. import loader
+
+class DotMod(loader.Module):
+	"""Данный модуль ставит точку в конце сообщения. Credit: @creative_tg1. Что я сделал? Запихнул модуль в ссылку, сделал команду только для заглавной буквы в начале."""
+	strings = {"name": "Dot"}
+	
+	async def dotplusoncmd(self, message):
+		""".dotpluson включает модуль Dot+."""
+		dot = 11
+		await message.edit("<b>Dot+ On.</b>")
+  async def dotoncmd(self, message):
+		""".doton включает модуль Dot."""
+		dot = 1
+		await message.edit("<b>Dot On.</b>")
+	async def dotplusoffcmd(self, message):
+		""".dotoff выключает модуль Dot+."""
+		dot = 21
+		await message.edit("<b>Dot+ Off.</b>")
+  async def dotoffcmd(self, message):
+		""".dotoff выключает модуль Dot."""
+		dot = 1
+		await message.edit("<b>Dot Off.</b>")
+	
+	async def watcher(self, message):
+		if dot == 11:
+			me = (await message.client.get_me())
+			if message.sender_id == me.id:
+				text = message.text.lower()
+				textup = text[0].upper()
+				textdown = text[1:]
+				txt = textup + textdown
+				lentxt = len(txt) - 1
+				if txt[lentxt] == "?" or txt[lentxt] == "!" or txt[lentxt] == ".":
+					await message.edit(txt)
+				else:
+					await message.edit(txt + ".")
+     if dot == 21:
+			me = (await message.client.get_me())
+			if message.sender_id == me.id:
+				text = message.text.lower()
+				textup = text[0].upper()
+				textdown = text[1:]
+				txt = textup + textdown
+				lentxt = len(txt) - 1
+				
