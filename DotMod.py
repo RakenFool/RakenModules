8598d98ee5b9f5d4e463943f4f5652865dc3a829
RakenFool <92396373+RakenFool@users.noreply.github.com>
2021-12-08 22:13:50 +0200

    Update DotMod.py

--- DotMod.py
+++ DotMod.py
@@ -3,27 +3,23 @@ from .. import loader
 class DotMod(loader.Module):
 	"""Данный модуль ставит точку в конце сообщения. Credit: @creative_tg1. Что я сделал? Запихнул модуль в ссылку, сделал команду только для заглавной буквы в начале."""
 	strings = {"name": "Dot"}
-	
+	dot = 0
 	async def dotplusoncmd(self, message):
 		""".dotpluson включает модуль Dot+."""
 		dot = 11
-		await message.edit("<b>Dot+ On.</b>")
-		
+		await message.edit("<b>Dot+ On.</b>")		
         async def dotoncmd(self, message):
 		""".doton включает модуль Dot."""
 		dot = 1
-		await message.edit("<b>Dot On.</b>")
-		
+		await message.edit("<b>Dot On.</b>")		
         async def dotplusoffcmd(self, message):
 		""".dotoff выключает модуль Dot+."""
 		dot = 21
-		await message.edit("<b>Dot+ Off.</b>")
-		
+		await message.edit("<b>Dot+ Off.</b>")		
         async def dotoffcmd(self, message):
 		""".dotoff выключает модуль Dot."""
 		dot = 1
-		await message.edit("<b>Dot Off.</b>")
-	
+		await message.edit("<b>Dot Off.</b>")	
 	async def watcher(self, message):
 		if dot == 11:
 			me = (await message.client.get_me())
@@ -36,8 +32,7 @@ class DotMod(loader.Module):
 				if txt[lentxt] == "?" or txt[lentxt] == "!" or txt[lentxt] == ".":
 					await message.edit(txt)
 				else:
-					await message.edit(txt + ".")
-					
+					await message.edit(txt + ".")					
                  elif dot == 21:
 			me = (await message.client.get_me())
 			if message.sender_id == me.id:
