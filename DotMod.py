b0b42dca4e27b6de2dd5eeb49deab2f5006f198b
RakenFool <92396373+RakenFool@users.noreply.github.com>
2021-12-11 15:26:28 +0200

    Update DotMod.py

--- DotMod.py
+++ DotMod.py
@@ -23,21 +23,21 @@ class DotMod(loader.Module):
 	async def watcher(self, message):
 		if dot == 11:
 			me = (await message.client.get_me())
-		if message.sender_id == me.id:
-			text = message.text.lower()
-			textup = text[0].upper()
-			textdown = text[1:]
-			txt = textup + textdown
-			lentxt = len(txt) - 1
-			if txt[lentxt] == "?" or txt[lentxt] == "!" or txt[lentxt] == ".":
-				await message.edit(txt)
+			if message.sender_id == me.id:
+				text = message.text.lower()
+				textup = text[0].upper()
+				textdown = text[1:]
+				txt = textup + textdown
+				lentxt = len(txt) - 1
+				if txt[lentxt] == "?" or txt[lentxt] == "!" or txt[lentxt] == ".":
+					await message.edit(txt)
 			elif dot == 21:
 				me = (await message.client.get_me())
-				if message.sender_id == me.id:
-					text = message.text.lower()
-					textup = text[0].upper()
-					textdown = text[1:]
-					txt = textup + textdown
-					lentxt = len(txt) - 1
+					if message.sender_id == me.id:
+						text = message.text.lower()
+						textup = text[0].upper()
+						textdown = text[1:]
+						txt = textup + textdown
+						lentxt = len(txt) - 1
 			else:
 				await message.edit(txt + ".")
