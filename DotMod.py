13d0bc94327522f1683fc18f52154d0c27aca716
RakenFool <92396373+RakenFool@users.noreply.github.com>
2021-12-11 14:41:31 +0200

    Update DotMod.py

--- DotMod.py
+++ DotMod.py
@@ -31,9 +31,7 @@ class DotMod(loader.Module):
 			lentxt = len(txt) - 1
 			if txt[lentxt] == "?" or txt[lentxt] == "!" or txt[lentxt] == ".":
 				await message.edit(txt)
-			else:
-				await message.edit(txt + ".")					
-       			elif dot == 21:
+			elif dot == 21:
 				me = (await message.client.get_me())
 				if message.sender_id == me.id:
 					text = message.text.lower()
@@ -41,4 +39,6 @@ class DotMod(loader.Module):
 					textdown = text[1:]
 			    		txt = textup + textdown
 			 		lentxt = len(txt) - 1
-				
+			else:
+				await message.edit(txt + ".")					
+			
