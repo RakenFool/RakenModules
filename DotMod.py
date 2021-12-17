f363ffda19fbd613e2a2bdd0dda7104d2dbcd7ca
RakenFool <92396373+RakenFool@users.noreply.github.com>
2021-12-17 19:18:18 +0200

    Update DotMod.py

--- DotMod.py
+++ DotMod.py
@@ -6,22 +6,14 @@ class DotMod(loader.Module):
 	dot = 0	
 	async def dotplusoncmd(self, message):
 		""".dotpluson включает модуль Dot+."""
-		dot = 11
-		await message.edit("<b>Dot+ On.</b>")	
-	async def dotoncmd(self, message):
-		""".doton включает модуль Dot."""
-		dot = 21
-		await message.edit("<b>Dot On.</b>")		
+		self.truefalse = True
+		await message.edit("<b>Dot+ On.</b>")			
 	async def dotplusoffcmd(self, message):
 		""".dotoff выключает модуль Dot+."""
-		dot = 1
+		self.truefalse = False
 		await message.edit("<b>Dot+ Off.</b>")		
-	async def dotoffcmd(self, message):
-		""".dotoff выключает модуль Dot."""
-		dot = 1
-		await message.edit("<b>Dot Off.</b>")	
 	async def watcher(self, message):
-		if dot == 11:			
+		if self.truefalse == True:			
 			me = (await message.client.get_me())
 			if message.sender_id == me.id:
 				text = message.text.lower()
@@ -33,10 +25,3 @@ class DotMod(loader.Module):
 					await message.edit(txt)
 				else:
 					await message.edit(txt + ".")
-		elif dot == 21:			
-			if message.sender_id == me.id:
-				text = message.text.lower()
-				textup = text[0].upper()
-				textdown = text[1:]
-				txt = textup + textdown
-				lentxt = len(txt) - 1
