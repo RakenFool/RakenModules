9d68e32cd4383147122b09555806a9be50234033
RakenFool <92396373+RakenFool@users.noreply.github.com>
2021-12-17 19:40:58 +0200

    Update DotMod.py

--- DotMod.py
+++ DotMod.py
@@ -3,15 +3,15 @@ from .. import loader
 class DotMod(loader.Module):
 	"""Данный модуль данный модуль делает первую букву сообщения заглавной. Credit: @creative_tg1. Что я сделал? Запихнул модуль в ссылку, сделал команду только для заглавной буквы в начале."""
 	strings = {"name": "Dot"}
-	dot = 0	
+	
 	async def dotplusoncmd(self, message):
-		""".dotpluson включает модуль Dot+."""
+		""".doton включает модуль Dot."""
 		self.truefalse = True
-		await message.edit("<b>Dot+ On.</b>")			
-	async def dotplusoffcmd(self, message):
-		""".dotoff выключает модуль Dot+."""
+		await message.edit("<b>Dot On.</b>")			
+	async def dotoffcmd(self, message):
+		""".dotoff выключает модуль Dot."""
 		self.truefalse = False
-		await message.edit("<b>Dot+ Off.</b>")		
+		await message.edit("<b>Dot Off.</b>")		
 	async def watcher(self, message):
 		if self.truefalse == True:			
 			me = (await message.client.get_me())
